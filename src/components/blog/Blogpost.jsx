3ce8e838021e9919098ae406505bbb3714b04130
haryobamy <haryobamy.badmus@gmail.com>
2021-07-27 01:56:25 +0100

    update

--- src/components/blog/Blogpost.jsx
+++ src/components/blog/Blogpost.jsx
@@ -11,6 +11,7 @@ import {
 
 const Blogpost = ({ bg, label }) => {
   return (
+
     <div className="blogspost">
       <img src={bg} alt="" className="blogpostimg" />
       <Button className="bg-light text-dark fw-bold postsButton ">
@@ -21,22 +22,23 @@ const Blogpost = ({ bg, label }) => {
         <h2>{label}</h2>
         <p>3 min Read . by Jessica</p>
         <h6>
-         
+
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
           dolorem error eos aut dolore, autem eligendi. At amet doloribus eius
           eos impedit qui non ut quam. Consequuntur, nihil quidem. Voluptatem.
         </h6>
         {/* <span><i className='fa fa-angle-right fa-2x'></i></span> */}
         <Button
-        className='moreButton '
-        color='dark'
+          className='moreButton '
+          color='dark'
           href="#"
-           
-           >
+
+        >
           <i className="fa fa-angle-right fa-3x  "></i>
-            </Button>
+        </Button>
       </div>
     </div>
+
   );
 };
 
